02af4b59857deda51f792e8005a9b7020ef2bc1c
14tobinj <Josh@Joshs-MacBook-Pro-5.local>
2014-05-26 14:52:30 -0400

    Revert "Trivia Quiz"
    This reverts commit a90b552913aa535726874e61e5229a3b5a265361.

--- Quiz/ViewController.m
+++ Quiz/ViewController.m
@@ -5,29 +5,19 @@
 //  Created by Josh Tobin on 5/25/14.
 //  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
 //
-/*
- Name: Josh Tobin
- Date: 5/26/2014
- Description:  The ViewController class is only used to handle 
- */
+
 #import "ViewController.h"
 
 @interface ViewController ()
-
-
 
 @end
 
 @implementation ViewController
 
-
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    
-    
 }
 
 - (void)didReceiveMemoryWarning
@@ -37,4 +27,3 @@
 }
 
 @end
-
